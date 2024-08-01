99aaeac9c434daa47f2987e21be712bfee65cf59
rohit10 <r.r0191@gmail.com>
2024-08-01 03:39:44 +0000

    Changes as per the class

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 function App() {
+  const name = "StarGazers";
   return (
+    <div className="container">
+    <article>
     <hgroup>
-      <h1>Meet the StarGazers</h1>
+      <img src="images/group.svg" alt="StarGazers group"></img>
+      <h1>Meet the <i style={{color:"darkseagreen"}}>{name}</i></h1>
       <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+    <button className="outline" onClick={()=>alert("Hi There")}>click me</button>
     </hgroup>
+    </article>
+    </div>
   )
 }
 export default App
